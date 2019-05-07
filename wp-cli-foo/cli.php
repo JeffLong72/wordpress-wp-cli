e36de3f258f94da64194241de0d4d49ab0c36739
JeffLong72 <45916412+JeffLong72@users.noreply.github.com>
2019-05-07 08:17:04 +0200

    Add files via upload

---
+++ wp-cli-foo/cli.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Class for foo
+ * Docs: https://make.wordpress.org/cli/handbook/commands-cookbook/
+ * $ wp foo [function-name] --key=value --key=value
+ * ( eg. $ wp foo test --name='Joe bloggs' --email='joe@bloggs.com')
+ *
+ * Or to call the wp-cli.phar directly ( eg. in situations where you cannot convert "wp-cli.phar" to executable file "wp" )
+ * $ php /root/path/to/wp-cli.phar foo test
+ * - or -
+ * $ php73.bin.cli /root/path/to/wp-cli.phar foo test
+ * ( but this is usually not necessary because PHP automatically selects the "correct" interpreter )
+ *
+ */
+ 
+class WP_CLI_Foo extends WP_CLI_Command {
+
+    public function __invoke( $args, $assoc_args ) 
+	{
+		switch($args[0]) 
+		{
+			case "test":
+				$this->test($assoc_args);
+				break;
+			default:
+				WP_CLI::error( "Missing [function-name], try using the command: $ wp foo test", $exit = true );
+		}
+    }
+	
+	public function test($assoc_args) 
+	{
+		WP_CLI::success( "This message is displayed from inside the test function! " );
+		
+		if(!empty($assoc_args)) 
+		{
+			WP_CLI::line( "Displaying all assoc_args values...");
+			
+			foreach($assoc_args as $key => $value) 
+			{
+				WP_CLI::log( $key."=>".$value );
+			}
+		}
+	}
+
+}	
+WP_CLI::add_command( 'foo', 'WP_CLI_Foo' );	 
+
---
+++ wp-cli-foo/wp-cli-foo.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ Plugin Name: WP-CLI Foo
+ Plugin URI:  
+ Description: A WP-CLI addon command for testing purposes. Use <strong>$ wp foo [function-name]</strong> to call a specific function. You can also pass multiple key=value params to the function by using <strong>$ wp foo [function-name] --key=value  --key=value</strong>
+ Version: 1.0.0
+ Author:      ACC Digital (Jeff Long)
+ Author URI:  https://www.acc.cc/
+ License: GPLv2+
+ */
+
+/**
+ * Load extension if WP CLI exists.
+ */
+function wp_cli_foo_load() {
+	
+	if ( defined( 'WP_CLI' ) && WP_CLI ) 
+	{
+		include dirname(__FILE__) . '/cli.php';
+	}
+}
+add_action( 'plugins_loaded', 'wp_cli_foo_load' );
